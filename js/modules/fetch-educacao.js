2c1d8795338a16d99a9e09c98bf76c5f548515d6
luwanhika <luwanhika@hotmail.com>
2021-06-20 21:08:38 -0300

    manipulação js

---
+++ js/modules/fetch-educacao.js
@@ -0,0 +1,21 @@
+export default function initFetchEducacao() {
+
+}
+
+async function fetchEducacao(url) {
+  const educacaoResponse = await fetch(url);
+  const educacaoJSON = await educacaoResponse.json();
+
+  educacaoJSON.forEach(cidade => {
+    console.log(cidade.campus.city);
+    createEducacao(cidade);
+  });
+}
+
+function createEducacao(cidade) {
+  const div = document.createElement('div');
+  div.classList.add('select-form');
+  div.innerHTML = ``
+}
+
+fetchEducacao('./db.json');
---
+++ js/script.js
@@ -0,0 +1,8 @@
+import initModal from './modules/modal.js';
+import initFetchEducacao from './modules/fetch-educacao.js'
+
+
+
+
+initModal();
+initFetchEducacao();
